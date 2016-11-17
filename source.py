d8924e13833770611328c1e8b93acb1c58af2d2b
Shashwat Vikram <shashwat.vikram50@gmail.com>
2016-11-18 00:28:54 +0530

    Add Components 	- Ball, Paddle1, Paddle2, Arena

--- source.py
+++ source.py
@@ -18,9 +18,51 @@ class Screen:
 class Pong(Screen):
     def __init__(self):
         Screen.__init__(self)
+        self.LINE_THICKNESS = 8
+        self.PADDLE_SIZE = 50
+        self.PADDLE_OFFSET = 20
+
+    def init_components(self):
+        ##########
+        # INITIAL POSITIONS
+        ball_pos_X = (self.SCREEN_WIDTH - self.LINE_THICKNESS)//2
+        ball_pos_Y = (self.SCREEN_HEIGHT - self.LINE_THICKNESS)//2
+        paddle_pos_Y = (self.SCREEN_HEIGHT - self.PADDLE_SIZE)//2
+        ##########
+        # BALL, PADDLE1, PADDLE2
+        self.ball = pygame.Rect((ball_pos_X, ball_pos_Y), (self.LINE_THICKNESS, self.LINE_THICKNESS))
+        self.paddle1 = pygame.Rect((self.PADDLE_OFFSET, paddle_pos_Y), (self.LINE_THICKNESS, self.PADDLE_SIZE))
+        self.paddle2 = pygame.Rect((self.SCREEN_WIDTH - self.LINE_THICKNESS - self.PADDLE_OFFSET, paddle_pos_Y), (self.LINE_THICKNESS, self.PADDLE_SIZE))
+
+        self.draw_components()
+
+    def draw_ball(self, ball):
+        pygame.draw.rect(self.SCREEN, self.WHITE, ball)
+
+    def draw_paddle(self, paddle):
+        if paddle.top < self.LINE_THICKNESS:
+            paddle.top = self.LINE_THICKNESS
+        elif paddle.bottom > (self.SCREEN_HEIGHT - self.LINE_THICKNESS):
+            paddle.bottom = self.SCREEN_HEIGHT - self.LINE_THICKNESS
+        pygame.draw.rect(self.SCREEN, self.WHITE, paddle)
+
+    def draw_arena(self):
+        self.SCREEN.fill(self.BLACK)
+        # Center Line
+        pygame.draw.line(self.SCREEN, self.WHITE, (self.SCREEN_WIDTH//2, 0), (self.SCREEN_WIDTH//2, self.SCREEN_HEIGHT), self.LINE_THICKNESS//4)
+        # Draw Outline of Arena
+        pygame.draw.rect(self.SCREEN, self.WHITE, ((0, 0),(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), self.LINE_THICKNESS*2)
+
+    def draw_components(self):
+        self.draw_arena()
+        self.draw_paddle(self.paddle1)
+        self.draw_paddle(self.paddle2)
+        self.draw_ball(self.ball)
 
     def main(self):
         pygame.init()
+        self.init_components()
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
